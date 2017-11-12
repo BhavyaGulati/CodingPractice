9983670bc08aceabb9ef9ba4f33e468715c70840
Bhavya Gulati <bgulati@usc.edu>
2017-11-11 22:44:16 -0800

    Add files via upload

---
+++ AllPermutationsofString.py
@@ -0,0 +1,14 @@
+def permute(a, l, r):
+    if l==r:
+        print "".join(a)
+    else:
+        for i in xrange(l,r+1):
+            a[l], a[i] = a[i], a[l]
+            permute(a, l+1, r)
+            a[l], a[i] = a[i], a[l] # backtrack
+
+
+str="ABC"
+a=list(str)
+permute(a,0,len(str)-1)
+
